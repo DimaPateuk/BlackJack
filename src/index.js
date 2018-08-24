cd1eb931b1484662f5ebb82b38bfbb00ac8b6ded
Dzmitry Patseyuk <dzmitry_patseyuk@epam.com>
2018-08-24 13:03:57 +0300

    create simple classifier

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const fs = require("fs");
+const hog = require("hog-features");
+const { default: Image } = require("image-js");
+const SVM = require("libsvm-js/asm");
+const Kernel = require("ml-kernel");
+const createClassifier = require("./detector");
+
+(async () => {
+  var { getDescriptor, classifier } = await createClassifier();
+  var descriptor = await getDescriptor(path.join(__dirname, "detector/data/4_0.png"));
+  // var descriptor = await getDescriptor(path.join(__dirname, "test.jpg"));
+  var t = classifier.predictOne(descriptor);
+ console.log(11111111, t);
+})();
